aaa60edb2e37359db3f786fb8cf45508f412624d
BrandonKim <brandonkim@virginia.edu>
2020-11-04 23:44:25 -0500

    Added LinkedList folder

---
+++ CTCI/Linked_Lists/linkedListImplementation.js
@@ -0,0 +1,20 @@
+class LinkedListClass {
+    constructor() {
+        this.next = null;
+        
+    }
+};
+
+class NodeClass {
+    constructor(data) {
+        this.data = data;
+    }
+};
+
+function LinkedList() {
+
+};
+
+function Node() {
+
+};
